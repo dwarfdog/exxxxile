ae66487607f34e4cee0922f28c0d2c9defceec7e
badj62 <badj62@msn.com>
2019-06-19 10:44:22 +0200

    add smileys for precise_bbcode;dev work on buildings

--- pyxile/urls.py
+++ pyxile/urls.py
@@ -13,6 +13,8 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf.urls.static import static
+from django.conf import settings
 from django.contrib import admin
 from django.urls import include,path
 from nexus.views import index
@@ -22,4 +24,5 @@ urlpatterns = [
     path('exile/', include('exile.urls')),
     path('nexus/', include('nexus.urls')),
     path('admin/', admin.site.urls),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
