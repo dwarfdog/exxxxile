6d05a914e8e4d3d87d54e82df2203df1c9e2c32a
dwarfdog <47698012+dwarfdog@users.noreply.github.com>
2024-12-30 13:55:42 +0100

    Rewrite

--- nexus/static/nexus/scripts/common.js
+++ nexus/static/nexus/scripts/common.js
@@ -1,107 +1,126 @@
+// Obtenir le chemin du script actuel
 var scriptPath = location.href;
 var p = scriptPath.lastIndexOf('/');
-if(p >= 0) scriptPath = scriptPath.substring(0, p);
+if (p >= 0) scriptPath = scriptPath.substring(0, p);
 scriptPath += '/';
 
+// Obtenir le fichier du script actuel sans paramètres ou fragment
 var scriptFile = location.href;
-p = scriptFile.indexOf('?');
-if(p >= 0) scriptFile = scriptFile.substring(0, p);
-p = scriptFile.indexOf('#');
-if(p >= 0) scriptFile = scriptFile.substring(0, p);
+p = scriptFile.indexOf('?'); // Supprime la partie après le "?" (query string)
+if (p >= 0) scriptFile = scriptFile.substring(0, p);
+p = scriptFile.indexOf('#'); // Supprime la partie après le "#" (fragment)
+if (p >= 0) scriptFile = scriptFile.substring(0, p);
 
-
+// Récupérer la valeur d'un champ à partir de son sélecteur
 function getval(name) {
-	var obj = $(name);
-	if(obj == null) return 0;
+    var obj = document.querySelector(name); // Sélectionne l'élément correspondant
+    if (obj == null) return 0;
 
-	var s = parseInt(obj.value, 10);
-	if(isNaN(s) || (s < 0)) return 0; else return s;
+    var s = parseInt(obj.value, 10); // Convertit la valeur en entier
+    return isNaN(s) || s < 0 ? 0 : s; // Retourne 0 si la valeur est invalide
 }
 
+// Définir une valeur sur un champ à partir de son sélecteur
 function setval(name, val) {
-	$(name).value = val;
+    var obj = document.querySelector(name); // Sélectionne l'élément correspondant
+    if (obj) obj.value = val; // Définit la valeur si l'élément existe
 }
 
-function getWindowDimensions(){
-	scrollX = document.documentElement.scrollLeft || document.body.scrollLeft;
-	scrollY = document.documentElement.scrollTop || document.body.scrollTop;
+// Obtenir les dimensions de la fenêtre visible
+function getWindowDimensions() {
+    // Position de défilement horizontal et vertical
+    var scrollX = window.scrollX || document.documentElement.scrollLeft || 0;
+    var scrollY = window.scrollY || document.documentElement.scrollTop || 0;
 
-	if(window.innerWidth) return {width: scrollX+window.innerWidth + scrollX, height: scrollY+window.innerHeight};
-	if(document.documentElement && document.documentElement.clientHeight) return {width: scrollX+document.documentElement.clientWidth, height: scrollY+document.documentElement.clientHeight};
-	if(document.body) return {width: scrollX+document.body.clientWidth, height: scrollY+document.body.clientHeight};
+    // Largeur et hauteur de la fenêtre visible
+    var width = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
+    var height = window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight;
+
+    return { width: scrollX + width, height: scrollY + height };
 }
 
 /*
- * format functions 
+ * Fonction pour formater une chaîne de caractères
+ * Remplace les valeurs dans une chaîne basée sur un tableau d'arguments
+ * Exemple : format("Hello $name!", { name: "World" }) retourne "Hello World!"
  */
-
-// Returns a formatted string assembled from a format string and an array of values
-// s is the string to be formated
-// args is an array of values to replace in the format string
 function format(s, args) {
-	var rx = new RegExp('\\$([a-z0-9]+)(:[a-z]+|)', 'ig');
-	var arr = null;
+    var rx = new RegExp('\\$([a-z0-9]+)(:[a-z]+|)', 'ig');
+    var arr = null;
 
-	while(arr = rx.exec(s)) {
-		var value = args[arr[1]];
+    while ((arr = rx.exec(s))) {
+        var value = args[arr[1]]; // Récupère la valeur associée à la clé
 
-		switch(arr[2]){
-			case ':n':
-				value = formatNumber(value);
-				break;
-			case ':alliance':
-				value = '<a href="alliance.asp?tag=' + value.tag + '">[' + value.tag + '] ' + value.name + '</a>';
-				break;
-			case ':player':
-				value = '<a href="nation.asp?name=' + value + '">' + value + '</a>';
-				break;
-			case ':research':
-				value = Exile.data.researches[value].name;
-			default:
-				break;
-		}
+        // Applique des transformations en fonction des modificateurs
+        switch (arr[2]) {
+            case ':n': // Formate un nombre
+                value = formatNumber(value);
+                break;
+            case ':alliance': // Lien vers une alliance
+                value = `<a href="alliance.asp?tag=${value.tag}">[${value.tag}] ${value.name}</a>`;
+                break;
+            case ':player': // Lien vers un joueur
+                value = `<a href="nation.asp?name=${value}">${value}</a>`;
+                break;
+            case ':research': // Nom d'une recherche
+                value = Exile.data.researches[value]?.name || value;
+                break;
+            default:
+                break;
+        }
 
-		s = s.replace(arr[0], value);
-	}
+        s = s.replace(arr[0], value); // Remplace la chaîne formatée
+    }
 
-	return s;
+    return s;
 }
 
+// Ajouter des balises <wbr> pour permettre une coupure dans une chaîne longue
 var wbr = "<wbr/>";
-if(navigator.userAgent.indexOf("Opera") != -1) wbr = "&#8203;";	
+if (navigator.userAgent.indexOf("Opera") != -1) wbr = "&#8203;";
 
 function addWbr(str) {
-	if(str.length < 30) return str;
+    if (str.length < 30) return str; // Pas besoin de couper si la chaîne est courte
 
-	var s = "";
-	for(k=0;k<str.length;k+=2)
-		s = s + str.substr(k,2) + wbr;
-	return s;
+    var s = "";
+    for (var k = 0; k < str.length; k += 2) {
+        s += str.substr(k, 2) + wbr; // Ajoute <wbr> tous les deux caractères
+    }
+    return s;
 }
 
+// Ajouter des séparateurs de milliers dans un nombre
+function addThousands(nStr, outD = '.', sep = ' ') {
+    nStr += ''; // Convertit en chaîne de caractères
+    var dpos = nStr.indexOf('.'); // Trouve la position du séparateur décimal
+    var nStrEnd = '';
 
-function addThousands(nStr, outD, sep) {
-	nStr += '';
-	var dpos = nStr.indexOf(".");
-	var nStrEnd = '';
-	if (dpos != -1) {
-		nStrEnd = outD + nStr.substring(dpos + 1, nStr.length);
-		nStr = nStr.substring(0, dpos);
-	}
-	var rgx = /(\d+)(\d{3})/;
-	while (rgx.test(nStr)) {
-		nStr = nStr.replace(rgx, '$1' + sep + '$2');
-	}
-	return nStr + nStrEnd;
+    if (dpos != -1) {
+        nStrEnd = outD + nStr.substring(dpos + 1); // Garde la partie décimale
+        nStr = nStr.substring(0, dpos); // Garde la partie entière
+    }
+
+    var rgx = /(\d+)(\d{3})/; // Regex pour insérer les séparateurs
+    while (rgx.test(nStr)) {
+        nStr = nStr.replace(rgx, '$1' + sep + '$2'); // Ajoute le séparateur
+    }
+    return nStr + nStrEnd; // Retourne le nombre formaté
 }
 
+// Formater un nombre avec séparateurs de milliers
 function formatNumber(n) {
-	return addThousands(n,'.',' ');
+    return addThousands(n, '.', ' ');
 }
 
-Number.prototype.n = function() { return addThousands(this,'.',' '); }
-Number.prototype.lz = function() { return (this < 10?"0":"") + this; }
+// Ajouter des méthodes au prototype Number
+Number.prototype.n = function () {
+    return addThousands(this, '.', ' ');
+};
+Number.prototype.lz = function () {
+    return this < 10 ? `0${this}` : `${this}`;
+};
 
+// Créer une date UTC à partir d'un timestamp
 function UTCDate(x) {
-	return new Date(x - new Date().getTimezoneOffset()*60000);
+    return new Date(x - new Date().getTimezoneOffset() * 60000);
 }
--- nexus/templatetags/nexus_extras.py
+++ nexus/templatetags/nexus_extras.py
@@ -1,5 +1,6 @@
 from django import template
 from django.conf import settings
+from datetime import datetime
 
 register = template.Library()
 
@@ -7,3 +8,7 @@ register = template.Library()
 @register.simple_tag
 def settings_value(name):
     return getattr(settings, name, "")
+
+@register.simple_tag
+def annee_en_cours():
+    return datetime.now().year
